2d123caf2045bcc9b84117bf536b1ef4957d31c0
David Silvério <david.a.silverio@gmail.com>
2020-01-04 20:55:50 +0000

    added new module fucntion

--- python_project/led_array.py
+++ python_project/led_array.py
@@ -1,23 +1,26 @@
 import RPi.GPIO as GPIO
 
-led0 = 14
-led1 = 15
-led2 = 18
-led3 = 23
-led4 = 24
-led5 = 25
-led6 = 12
-led7 = 16
-led8 = 20
-led9 = 21
+leds = [18, 23, 24, 25, 8, 7, 12, 16, 20, 21]
 
 def initArrayLED():
-    GPIO.setup(led0, GPIO.OUT, initial=0)
+    for led in leds:
+        GPIO.setup(led, GPIO.OUT, initial=0)
 
-def setAllLEDS():
-    for i in range(9, 5, -1)
-        name = "led{}".format(i)
-        GPIO.output(name, 1)
+def setAllLedsHigh():
+    for led in leds:
+        GPIO.output(led, 1)
+    
+def setAllLedsLow():
+    for led in leds:
+        GPIO.output(led, 0)
 
-def setLED0():
-    GPIO.output(led0, 1)
+def setLEDHigh(num):
+    GPIO.output(leds[num], 1)
+
+
+def setLEDLow(num):
+    GPIO.output(leds[num], 0)
+
+def variableRangeLED(num):
+    for i in range(0, num)
+        GPIO.output(leds[num], 0)
